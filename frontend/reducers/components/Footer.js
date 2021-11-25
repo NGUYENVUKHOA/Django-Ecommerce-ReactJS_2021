a0e252c94e18d43163bcf055c6a56213cb3f9bfb
NGUYENVUKHOA <nguyenvukhoa31031997@gmail.com>
2021-11-25 16:58:53 +0700

    Add files via upload

---
+++ frontend/reducers/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/* REACT-BOOTSTRAP */
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+        
+          <Col className="text-center py-3">WEBSITE DE HOC THOI </Col>
+          <Col className="text-center py-3">This is a not for commercial - It a website for LEARN WEB </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
